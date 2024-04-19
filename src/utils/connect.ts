a0c8193bfbe275673b165856f2ec4691280e2207
“Anhunghezo” <anhkieu303252@gmail.com>
2024-04-19 09:42:27 +0700

    feat: add admin user when connect db

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import config from "config";
 import logger from "./logger";
+import UserModel from "../models/user.model";
 
 async function connect() {
   const dbUri = config.get<string>("dbUri");
@@ -10,6 +11,27 @@ async function connect() {
       dbName: config.get<string>("dbName"),
     });
     logger.info("DB connected");
+
+    try {
+      const adminUser = await UserModel.findOne({ role: "admin" });
+
+      if (!adminUser) {
+        await UserModel.create({
+          name: "Admin",
+          phone: "0912345678",
+          password: "@123Admin",
+          verify: true,
+          role: "admin",
+          email: "getit.admin@gmail.com",
+        });
+        logger.info("Admin user created");
+      } else {
+        logger.info("Admin user already exists");
+      }
+    } catch (error) {
+      logger.error("Could not create admin user");
+      console.error(error);
+    }
   } catch (error) {
     logger.error("Could not connect to db");
     process.exit(1);
