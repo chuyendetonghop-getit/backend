310f5c29df4b85111c4606979369dd9dcacb379f
B460M <anhkieu303252@gmail.com>
2024-06-04 01:40:32 +0700

    feat: add find and update report service & update createReportHandler

--- src/controller/report.controller.ts
+++ src/controller/report.controller.ts
@@ -4,7 +4,11 @@ import { HttpStatusCode } from "axios";
 import { Request, Response } from "express";
 
 import { CreateReportInput } from "../schema/report.schema";
-import { createReport } from "../service/report.service";
+import {
+  createReport,
+  findReport,
+  updateReport,
+} from "../service/report.service";
 import logger from "../utils/logger";
 import {
   sendErrorResponse,
@@ -17,15 +21,39 @@ export async function createReportHandler(
 ) {
   const reporterId = res.locals.user._id;
   console.log("reporterId", reporterId);
+  const { postId, reason } = req.body;
   try {
-    const report = await createReport({ ...req.body, reporterId: reporterId });
+    // 1. Find if the report already exists in the database. By postId and reporterId.
+    const exportFile = await findReport({
+      postId: postId,
+      reporterId: reporterId,
+    });
 
-    return sendSuccessResponse(
-      res,
-      report,
-      HttpStatusCode.Created,
-      "Create report success"
-    );
+    if (exportFile) {
+      // 2. If the report exists, update the reason
+      const updatedReport = await updateReport(
+        { postId: postId, reporterId: reporterId },
+        { reason: reason }
+      );
+      return sendSuccessResponse(
+        res,
+        updatedReport,
+        HttpStatusCode.Ok,
+        "Update report success"
+      );
+    } else {
+      // 3. If the report does not exist, create a new report
+      const report = await createReport({
+        ...req.body,
+        reporterId: reporterId,
+      });
+      return sendSuccessResponse(
+        res,
+        report,
+        HttpStatusCode.Created,
+        "Create report success"
+      );
+    }
   } catch (e: any) {
     logger.error(e);
     return sendErrorResponse(res, HttpStatusCode.NotFound, e.message);
--- src/service/report.service.ts
+++ src/service/report.service.ts
@@ -1,6 +1,7 @@
 // report.service.ts
 
-import ReportModel from "../models/report.model";
+import { FilterQuery } from "mongoose";
+import ReportModel, { ReportDocument } from "../models/report.model";
 import { CreateReportInput } from "../schema/report.schema";
 
 export async function createReport(input: CreateReportInput["body"]) {
@@ -10,4 +11,17 @@ export async function createReport(input: CreateReportInput["body"]) {
   } catch (error) {
     throw new Error("Error when create report");
   }
+}
+
+// find report by postId and reporterId
+export async function findReport(query: FilterQuery<ReportDocument>) {
+  return ReportModel.findOne(query).lean();
+}
+
+// update report
+export async function updateReport(
+  query: FilterQuery<ReportDocument>,
+  update: Pick<CreateReportInput["body"], "reason">
+) {
+  return ReportModel.findOneAndUpdate(query, update, { new: true }).lean();
 }
